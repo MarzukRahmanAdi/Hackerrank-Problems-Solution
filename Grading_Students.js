9613f4e2e3c4b7b1745797470ad149dfdc0145af
Adi <aditto2001@gmail.com>
2020-12-19 18:55:23 +0600

    first

---
+++ Grading_Students.js
@@ -0,0 +1,122 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function gradingStudents(grades) {
+
+    var result = []
+    grades.map((grade) =>{
+        var arrEle = grade
+        if(grade > 37){
+            var gradeRemainer = grade % 5
+            if(gradeRemainer > 2){
+                arrEle =  grade + (5 - gradeRemainer)
+            }
+            
+        }
+        result.push(arrEle)
+    })
+    return result
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const gradesCount = parseInt(readLine().trim(), 10);
+
+    let grades = [];
+
+    for (let i = 0; i < gradesCount; i++) {
+        const gradesItem = parseInt(readLine().trim(), 10);
+        grades.push(gradesItem);
+    }
+
+    const result = gradingStudents(grades);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function gradingStudents(grades) {
+
+    var result = []
+    grades.map((grade) =>{
+        var arrEle = grade
+        if(grade > 37){
+            var gradeRemainer = grade % 5
+            if(gradeRemainer > 2){
+                arrEle =  grade + (5 - gradeRemainer)
+            }
+            
+        }
+        result.push(arrEle)
+    })
+    return result
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const gradesCount = parseInt(readLine().trim(), 10);
+
+    let grades = [];
+
+    for (let i = 0; i < gradesCount; i++) {
+        const gradesItem = parseInt(readLine().trim(), 10);
+        grades.push(gradesItem);
+    }
+
+    const result = gradingStudents(grades);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
