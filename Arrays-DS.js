91a2380fa551c67943345b5639bee4285f2b54cc
Adi <ironsharkadi2020@gmail.com>
2021-02-24 23:51:44 +0600

    Update Arrays-DS.js

--- Arrays-DS.js
+++ Arrays-DS.js
@@ -1,78 +1,45 @@
-#include <bits/stdc++.h>
+'use strict';
 
-using namespace std;
+const fs = require('fs');
 
-vector<string> split_string(string);
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
 
 // Complete the reverseArray function below.
-vector<int> reverseArray(vector<int> a) {
+function reverseArray(a) {
 
 
 }
 
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    int arr_count;
-    cin >> arr_count;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    const arrCount = parseInt(readLine(), 10);
 
-    string arr_temp_temp;
-    getline(cin, arr_temp_temp);
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
 
-    vector<string> arr_temp = split_string(arr_temp_temp);
+    const res = reverseArray(arr);
 
-    vector<int> arr(arr_count);
+    ws.write(res.join(' ') + '\n');
 
-    for (int i = 0; i < arr_count; i++) {
-        int arr_item = stoi(arr_temp[i]);
-
-        arr[i] = arr_item;
-    }
-
-    vector<int> res = reverseArray(arr);
-
-    for (int i = 0; i < res.size(); i++) {
-        fout << res[i];
-
-        if (i != res.size() - 1) {
-            fout << " ";
-        }
-    }
-
-    fout << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
+    ws.end();
 }
